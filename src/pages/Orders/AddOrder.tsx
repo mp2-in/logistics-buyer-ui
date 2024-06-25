3620417edde1fb59efc991863cdf1c1ce1daa9f9
Charan <charanraj12@gmail.com>
2024-06-25 12:22:16 +0530

    Reset state for add order form on close

--- src/pages/Orders/AddOrder.tsx
+++ src/pages/Orders/AddOrder.tsx
@@ -20,13 +20,18 @@ interface State {
     phoneNumber: string,
     value: string,
     rto: boolean,
-    store: string
+    store: string,
+    type: string
 }
 
-const reducer = (state: State, payload: Partial<State>) => {
-    return {
-        ...state,
-        ...payload
+const initialValue: State = { placesResponse: [], address: '', placeId: '', name: '', phoneNumber: '', value: '', rto: false, store: '', type: '' }
+
+const reducer = (state: State, action: { type: 'reset' } | { type: 'update', payload: Partial<State> }) => {
+    switch (action.type) {
+        case "update":
+            return { ...state, ...action.payload }
+        case "reset":
+            return initialValue
     }
 }
 
@@ -34,12 +39,13 @@ export default ({ open, onClose, onPlacesSearch, getPickupList, activity, pickup
     open: boolean, onClose: () => void, onPlacesSearch: (searchText: string,
         callback: (data: Place[]) => void) => void, getPickupList: () => void, activity: { [k: string]: boolean }, pickupStores: PickupStore[]
 }) => {
-    const [state, dispatch] = useReducer(reducer, { placesResponse: [], address: '', placeId: '', name: '', phoneNumber: '', value: '', rto: false, store: '' })
+    const [state, dispatch] = useReducer(reducer, initialValue)
 
     useEffect(() => {
         if (open) {
             getPickupList()
         }
+        dispatch({ type: 'reset' })
     }, [open])
 
     return <Modal open={open} onClose={onClose} loading={activity.getPickupList}>
@@ -50,44 +56,46 @@ export default ({ open, onClose, onPlacesSearch, getPickupList, activity, pickup
             </div>
             <div className={styles.body}>
                 <div>
-                    <Select label="Outlet" options={pickupStores.map(e => ({ label: e.address.name, value: e.storeId }))} onChange={val => dispatch({store: val})} value={state.store}/>
+                    <Select label="Outlet" options={pickupStores.map(e => ({ label: e.address.name, value: e.storeId }))} onChange={val => dispatch({ type: 'update', payload: { store: val } })} value={state.store} />
                     <p className={styles.link}>Add Outlet</p>
                 </div>
                 <p className={styles.sectionHeader}>Drop</p>
                 <div className={styles.dropDetails}>
-                    <Input label="Name" value={state.name} onChange={val => dispatch({ name: val })} />
-                    <Input label="Phone Number" size="small" value={state.phoneNumber} onChange={val => /^[0-9]*$/.test(val) && dispatch({ phoneNumber: val })} />
+                    <Input label="Name" value={state.name} onChange={val => dispatch({ type: 'update', payload: { name: val } })} />
+                    <Input label="Phone Number" size="small" value={state.phoneNumber} onChange={val => /^[0-9]*$/.test(val) && dispatch({ type: 'update', payload: { phoneNumber: val } })} />
                 </div>
                 <div className={styles.address}>
                     <Input label="Address" onChange={val => {
-                        dispatch({ address: val })
+                        dispatch({ type: 'update', payload: { address: val } })
                         if (val.length > 2) {
                             onPlacesSearch(val, (data) => {
                                 dispatch({
-                                    placesResponse: data.map(e => {
-                                        return {
-                                            id: e.id,
-                                            name: e.displayName.text,
-                                            address: e.formattedAddress,
-                                            latitude: e.location.latitude,
-                                            longitude: e.location.longitude
-                                        }
-                                    })
+                                    type: 'update', payload: {
+                                        placesResponse: data.map(e => {
+                                            return {
+                                                id: e.id,
+                                                name: e.displayName.text,
+                                                address: e.formattedAddress,
+                                                latitude: e.location.latitude,
+                                                longitude: e.location.longitude
+                                            }
+                                        })
+                                    }
                                 })
                             })
                         }
                     }} autoCompleteOptions={state.placesResponse.map(e => ({ label: `${e.name.toString()} (${e.address.toString()})`, value: e.id.toString() }))} size="extraLarge" value={state.address} onSelect={(l, v) => {
-                        dispatch({ address: l, placeId: v })
+                        dispatch({ type: 'update', payload: { address: l, placeId: v } })
                     }} />
                 </div>
                 <p className={styles.sectionHeader}>Order  Details</p>
                 <div>
-                    <Select label="Type" />
-                    <Input label="Value" size="small" value={state.value} onChange={val => /^[0-9]*$/.test(val) && dispatch({ value: val })} />
+                    <Select label="Type" options={[{label: 'Food', value: 'food'}, {label: 'Grocery', value: 'grocery'}]} onChange={val => dispatch({type: 'update', payload: ({type: val})})} value={state.type}/>
+                    <Input label="Value" size="small" value={state.value} onChange={val => /^[0-9]*$/.test(val) && dispatch({ type: 'update', payload: { value: val } })} />
                 </div>
                 <div className={styles.rto}>
                     <p>RTO Required: </p>
-                    <Switch on={state.rto} onClick={() => dispatch({ rto: !state.rto })} />
+                    <Switch on={state.rto} onClick={() => dispatch({ type: 'update', payload: { rto: !state.rto } })} />
                 </div>
                 <div>
                     <Select label="LSP" />
