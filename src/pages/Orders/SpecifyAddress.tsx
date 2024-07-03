0c8f0fa3261d490c000c3271af63dba1517963dd
Charan <charanraj12@gmail.com>
2024-07-03 13:29:45 +0530

    add order form ui update

--- src/pages/Orders/SpecifyAddress.tsx
+++ src/pages/Orders/SpecifyAddress.tsx
@@ -59,14 +59,18 @@ export default ({ onUpdate, onPlacesSearch, payload }: {
                 }} />
         </div> : <div className={'flex flex-col items-start *:mb-3'}>
             <Input label="Address Line 1" value={payload.addrLine1} onChange={val => onUpdate({ addrLine1: val })} />
-            <Input label="Address Line 2" value={payload.addrLine2} onChange={val => onUpdate({ addrLine2: val })} />
-            <div className={'flex items-center'}>
-                <Input label="City" value={payload.city} onChange={val => onUpdate({ city: val })} size='small' />
+            <div className="flex items-center">
+                <Input label="Address Line 2" value={payload.addrLine2} onChange={val => onUpdate({ addrLine2: val })} />
                 <div className="ml-3">
-                    <Select label="State" value={payload.state} onChange={val => onUpdate({ state: val })} options={getStates().map(e => ({ label: e, value: e }))} />
+                    <Input label="City" value={payload.city} onChange={val => onUpdate({ city: val })} size='small' />
                 </div>
             </div>
-            <Input label="Pincode" value={payload.pincode} onChange={val => /^[0-9]{0,6}$/.test(val) && onUpdate({ pincode: val })} size='small' />
+            <div className={'flex items-center'}>
+                <div className="mr-3">
+                    <Select label="State" value={payload.state} onChange={val => onUpdate({ state: val })} options={getStates().map(e => ({ label: e, value: e }))} />
+                </div>
+                <Input label="Pincode" value={payload.pincode} onChange={val => /^[0-9]{0,6}$/.test(val) && onUpdate({ pincode: val })} size='small' />
+            </div>
             <div className={'flex items-center'}>
                 <Input label="Geolocation" value={payload.geoLocation} onChange={val => onUpdate({ geoLocation: val })} />
                 {/^[0-9.]+\s*,\s*[0-9.]+$/.test(payload.geoLocation) ? <a className="ml-3 mt-3 font-bold underline cursor-pointer text-blue-500" href={`https://maps.google.com/?q=${payload.geoLocation}`} target="_blank">Maps Link</a> :
