0105659d2f169e8bd63ba3b2104416331884b949
Charan <charanraj12@gmail.com>
2024-07-11 07:06:50 +0530

    Added sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 // import './index.css'
+
+Sentry.init({
+  dsn: "https://4551ed53e6c2b72b13b9132c64393205@o4507575594713088.ingest.us.sentry.io/4507575596810240",
+  integrations: [],
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
