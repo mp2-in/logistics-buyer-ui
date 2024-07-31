ec7575fa71d5218a7376b4f14681072bd77464c2
Charan <charanraj12@gmail.com>
2024-07-31 09:22:33 +0530

    Row background based on order state

--- src/pages/Orders/OrderList.tsx
+++ src/pages/Orders/OrderList.tsx
@@ -30,6 +30,10 @@ export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, acti
         return ['Pending', 'Accepted', 'UnFulFilled', 'Searching-for-Agent', 'Agent-assigned', 'At-pickup', 'UnFulfilled'].includes(orderState)
     }
 
+    const rowBackground = (orderState: string) => {
+        return orderState === 'Order-delivered' ? 'bg-green-200' : orderState === 'Cancelled' ? 'bg-red-200' : ''
+    }
+
     useEffect(() => {
         const interval = setInterval(() => {
             onRefresh()
@@ -78,20 +82,20 @@ export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, acti
         </div>
         <div className={`absolute flex items-center flex-col left-2 right-2 bottom-2 top-[148px] overflow-auto lg:left-5 lg:right-5 lg:top-[123px] md:top-[110px]`}>
             {orders.map(eachOrder => {
-                return <div key={eachOrder.orderId} className={`flex items-center w-full py-1 px-1 border-b border-l border-r text-xs relative *:text-center lg:text-sm xl:*:mx-2`}>
+                return <div key={eachOrder.orderId} className={`flex items-center w-full py-1 px-1 border-b border-l border-r text-xs relative *:text-center lg:text-sm xl:*:mx-2 ${rowBackground(eachOrder.orderState)}`}>
                     <p className={`flex-[4] ml-0`}>{eachOrder.createdAt ? dayjs(eachOrder.createdAt).format('hh:mm A') : '--'}</p>
-                    <input className={`flex-[4] hidden xl:block  border-none outline-none w-full`} readOnly value={eachOrder.orderId} />
-                    <input className={`flex-[4] hidden xl:block  border-none outline-none w-full`} readOnly value={eachOrder.providerId} />
+                    <input className={`flex-[4] hidden xl:block  border-none outline-none w-full ${rowBackground(eachOrder.orderState)}`} readOnly value={eachOrder.orderId} />
+                    <input className={`flex-[4] hidden xl:block  border-none outline-none w-full ${rowBackground(eachOrder.orderState)}`} readOnly value={eachOrder.providerId} />
                     <p className={`flex-[2] hidden xl:block`} >{eachOrder.pcc}</p>
                     <p className={`flex-[2] hidden xl:block`} >{eachOrder.dcc}</p>
-                    <input className={`flex-[5] xl:flex[2] border-none outline-none w-full`} readOnly value={eachOrder.orderState} />
+                    <input className={`flex-[5] xl:flex[2] border-none outline-none w-full ${rowBackground(eachOrder.orderState)}`} readOnly value={eachOrder.orderState} />
                     <div className={`flex-[4] xl:block  hidden`}>
                         <p className='text-xs'>{eachOrder.dropName}</p>
                         <p className='text-xs'>{eachOrder.dropPhone}</p>
                     </div>
                     {eachOrder.riderNumber ? <a className={`flex-[4] underline cursor-pointer font-semibold text-blue-400`} href={`tel:${eachOrder.riderNumber}`}>{eachOrder.riderName}</a> :
                         <p className={`flex-[4]`}>{eachOrder.riderName}</p>}
-                    {eachOrder.distance ? <a className={`flex-[3] hidden xl:block text-blue-400 underline font-semibold`} href={mapLink(eachOrder) || ''} target='_blank'>{`${eachOrder.distance.toFixed(2)} km`}</a> :
+                    {eachOrder.distance ? <a className={`flex-[3] hidden xl:block text-blue-600 underline font-semibold`} href={mapLink(eachOrder) || ''} target='_blank'>{`${eachOrder.distance.toFixed(2)} km`}</a> :
                         <p className={`flex-[3] hidden xl:block`}>0</p>}
                     <p className={`flex-[3] hidden xl:block`}>{getPrice(eachOrder) ? `₹ ${getPrice(eachOrder).toFixed(2)}` : 0}</p>
                     <p className={`flex-[4] hidden xl:block`}>{eachOrder.deliveredAt ? dayjs(eachOrder.deliveredAt).format('hh:mm A') : '--'}</p>
