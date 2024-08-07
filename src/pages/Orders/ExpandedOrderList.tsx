a9228bdcda324a901a2c72763eff1125a417e05a
Charan <charanraj12@gmail.com>
2024-08-07 10:20:46 +0530

    sort in expanded page

--- src/pages/Orders/ExpandedOrderList.tsx
+++ src/pages/Orders/ExpandedOrderList.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import addIcon from "@assets/add.png"
 import cancelIcon from "@assets/cancel.png"
@@ -8,12 +8,27 @@ import warningIcon from "@assets/warning.png"
 import refreshIcon from "@assets/refresh.png"
 import trackIcon from "@assets/track.png"
 import ActivityIndicator from '@components/ActivityIndicator';
+import sortBlackDownIcon from "@assets/sort_black_down.png"
+import sortBlackUpIcon from "@assets/sort_black_up.png"
+import sortGreyDownIcon from "@assets/sort_grey_down.png"
+import sortGreyUpIcon from "@assets/sort_grey_up.png"
 
 import { Order } from '@lib/interfaces'
 
 import Input from '@components/Input';
 import Button from '@components/Button';
 import { cancellable } from '@lib/utils';
+
+
+const HeaderField = ({ cssClass, label, sort, hidden, onClick }: { cssClass: string, label: string, sort?: 'asc' | 'dsc', hidden?: boolean, onClick: () => void }) => {
+    return <div className={`${cssClass} ${hidden ? 'hidden xl:flex' : 'flex'} items-center cursor-pointer justify-center`} onClick={onClick}>
+        <p>{label}</p>
+        <div className={'flex-col items-center ml-2'}>
+            <img src={sort === 'asc' ? sortBlackUpIcon : sortGreyUpIcon} className='w-2 mb-1' />
+            <img src={sort === 'dsc' ? sortBlackDownIcon : sortGreyDownIcon} className='w-2' />
+        </div>
+    </div>
+}
 
 
 export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, activity, filterDate, chooseOrder, onIssueReport, isRetail }: {
@@ -28,6 +43,9 @@ export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, acti
     onIssueReport: (orderId: string) => void
     isRetail: boolean
 }) => {
+
+    const [sortOrder, setSortOrder] = useState<'asc' | 'dsc'>('dsc')
+    const [sortField, setSortField] = useState<keyof Order>('createdAt')
 
     const rowBackground = (orderState: string) => {
         return orderState === 'Order-delivered' ? 'bg-green-100' : orderState === 'Cancelled' ? 'bg-red-100' : ''
@@ -54,6 +72,19 @@ export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, acti
         return 0
     }
 
+    const sortOrders = (a: Order, b: Order) => {
+        return (a[sortField] || '') > (b[sortField] || '') ? sortOrder === 'asc' ? 1 : -1 : (a[sortField] || '') < (b[sortField] || '') ? sortOrder === 'asc' ? -1 : 1 : 0
+    }
+
+    const updateSortField = (field: keyof Order) => {
+        if(field === sortField) {
+            setSortOrder(sortOrder === 'asc' ? 'dsc' : 'asc')
+        } else {
+            setSortField(field)
+            setSortOrder('dsc')
+        }
+    }
+
     return <div className={`absolute left-0 right-0 top-12 bottom-3 lg:px-5 lg:py-3 px-2 sm:top-16`}>
         <div className={`flex sm:items-end items-start justify-between p-2 sm:flex-row-reverse flex-col mb-2`}>
             <div className={`flex flex-row-reverse w-full mb-3 sm:mb-0`}>
@@ -66,21 +97,21 @@ export default ({ onAddOrder, onRefresh, changeDate, onCancelOrder, orders, acti
         </div>
         <div className='absolute top-[110px] left-1 right-1 bottom-1 overflow-auto'>
             <div className={`flex items-center bg-blue-300 *:text-center *:font-semibold  xl:*:mx-2 *:text-xs md:*:text-base *:flex-shrink-0 w-full`}>
-                <p className={`w-[100px] ml-0 bg-blue-300 py-2 pl-1`}>Created At</p>
-                <p className={`w-[150px] bg-blue-300 py-2`}>Order Id</p>
-                <p className={`w-[150px] bg-blue-300 py-2`}>LSP</p>
+                <HeaderField cssClass='w-[100px] ml-0 bg-blue-300 py-2 pl-1' label='Creation' sort={sortField === 'createdAt' ? sortOrder : undefined} onClick={() => updateSortField('createdAt')} />
+                <HeaderField cssClass='w-[150px] bg-blue-300 py-2' label='Order Id' sort={sortField === 'orderId' ? sortOrder : undefined} onClick={() => updateSortField('orderId')} />
+                <HeaderField cssClass='w-[150px] ml-0 bg-blue-300 py-2 pl-1' label='LSP' sort={sortField === 'providerId' ? sortOrder : undefined} onClick={() => updateSortField('providerId')} />
                 <p className={`w-[100px] bg-blue-300 py-2`}>PCC</p>
                 <p className={`w-[100px] bg-blue-300 py-2`}>DCC</p>
-                <p className={`w-[150px] bg-blue-300 py-2`}>Status</p>
-                <p className={`w-[160px] bg-blue-300 py-2`}>Customer</p>
+                <HeaderField cssClass='w-[150px] bg-blue-300 py-2' label='Status' sort={sortField === 'orderState' ? sortOrder : undefined} onClick={() => updateSortField('orderState')} />
+                <HeaderField cssClass='w-[160px] bg-blue-300 py-2' label='Customer' sort={sortField === 'dropName' ? sortOrder : undefined} onClick={() => updateSortField('dropName')} />
                 <p className={`w-[150px] bg-blue-300 py-2`}>Rider</p>
-                <p className={`w-[100px] bg-blue-300 py-2`}>Distance</p>
-                <p className={`w-[100px] bg-blue-300 py-2`}>Price</p>
-                <p className={`w-[100px] bg-blue-300 py-2`}>Delivered At</p>
+                <HeaderField cssClass='w-[100px] bg-blue-300 py-2' label='Distance' sort={sortField === 'distance' ? sortOrder : undefined} onClick={() => updateSortField('distance')} />
+                <HeaderField cssClass='w-[100px] bg-blue-300 py-2' label='Price' sort={sortField === 'totalDeliveryCharge' ? sortOrder : undefined} onClick={() => updateSortField('totalDeliveryCharge')} />
+                <HeaderField cssClass='w-[100px] bg-blue-300 py-2' label='Delivered At' sort={sortField === 'deliveredAt' ? sortOrder : undefined} onClick={() => updateSortField('deliveredAt')} />
                 <p className={`w-[160px] mr-0 bg-blue-300 py-2 pr-1`}>Actions</p>
             </div>
             <div className={`absolute flex items-center flex-col left-0 right-0 bottom-0 top-[30px] lg:left-5 lg:right-5 lg:top-[123px] md:top-[110px] w-full`}>
-                {orders.map(eachOrder => {
+                {[...orders].sort(sortOrders).map(eachOrder => {
                     return <div key={eachOrder.orderId} className={`flex items-center w-full text-xs relative *:text-center lg:text-sm xl:*:mx-2 ${rowBackground(eachOrder.orderState)}  *:flex-shrink-0 h-[40px] w-full`}>
                         <p className={`w-[100px] ml-0 ${rowBackground(eachOrder.orderState)}`}>{eachOrder.createdAt ? dayjs(eachOrder.createdAt).format('hh:mm A') : '--'}</p>
                         <div className={`w-[150px] ${rowBackground(eachOrder.orderState)}`}>
