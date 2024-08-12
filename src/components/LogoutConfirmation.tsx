b79e8634ba6d3de9c52d5966ec9e0abbc187dcdd
Charan <charanraj12@gmail.com>
2024-08-12 13:06:34 +0530

    added orders and logout to menu

---
+++ src/components/LogoutConfirmation.tsx
@@ -0,0 +1,30 @@
+import Modal from "@components/Modal"
+import closeIcon from '@assets/close.png'
+import Button from "@components/Button"
+
+
+export default ({ open, onClose, logout, loading }: {
+    open: boolean,
+    onClose: () => void,
+    logout: () => void,
+    loading: boolean
+}) => {
+
+    return <Modal open={open} onClose={onClose}>
+        <div className={'bg-white rounded py-[10px] px-[20px] md:w-[600px] w-[350px] relative'} onMouseDown={e => e.stopPropagation()}>
+            <div className={'flex justify-between w-full items-center mb-[10px]'}>
+                <p className="text-xl font-semibold">Logout</p>
+                <img src={closeIcon} onClick={onClose} className="w-6 absolute top-1 right-1 cursor-pointer" />
+            </div>
+            <div className={'mt-1'}>
+                <p className="ml-3">Are you sure you want to logout?</p>
+                <div className="mt-[20px] mb-[15px] w-full flex justify-end">
+                    <Button title="Cancel" variant="light" onClick={onClose} disabled={loading} />
+                    <div className="ml-3">
+                        <Button title="Logout" variant="primary" onClick={logout} loading={loading} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Modal>
+}   
