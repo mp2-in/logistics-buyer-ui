43d382b5bfc132bb6acc407fae25ee5e1692f992
Charan <charanraj12@gmail.com>
2024-08-19 14:26:34 +0530

    Changed menu title

--- src/pages/Billing/index.tsx
+++ src/pages/Billing/index.tsx
@@ -23,7 +23,7 @@ export default () => {
     console.log(billingInfoLink, activity.getWalletDashboardLink)
 
     return <div>
-        <TopBar title='Billing' />
+        <TopBar title='Reports' />
         <div className={`absolute left-0 right-0 md:top-[80px] top-[50px] bottom-3 md:px-5 md:py-3 px-2`}>
             <div className={`absolute left-2 right-2 bottom-2 top-[60px] overflow-auto lg:left-5 lg:right-5 md:top-[85px]`}>
                 {billingInfoLink ? <div onMouseDown={e => e.stopPropagation()}>
