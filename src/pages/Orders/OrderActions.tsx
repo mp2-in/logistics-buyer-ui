ef3b5239659fe119c80a60316b82eb40dfea5cb9
Charan <charanraj12@gmail.com>
2024-09-09 11:52:30 +0530

    ui opacity fix on invalid order

--- src/pages/Orders/OrderActions.tsx
+++ src/pages/Orders/OrderActions.tsx
@@ -34,8 +34,8 @@ export default ({ orderInfo, onCancelOrder, onIssueReport, onOrderFulfillment, o
                 onIssueReport(orderInfo.orderId)
             }
         }}>
-            <div className="w-10 h-10 rounded-full border-2 border-red-500 flex items-center justify-center cursor-pointer"><img src={issueIcon} className="w-6" /></div>
-            <p className="text-sm mt-1 hidden md:block text-red-500 font-semibold">Issue</p>
+            <div className={`w-10 h-10 rounded-full border-2 border-red-500 flex items-center justify-center ${orderInfo?.orderId ? 'cursor-pointer' : 'opacity-30'}`}><img src={issueIcon} className="w-6" /></div>
+            <p className={`text-sm mt-1 hidden md:block text-red-500 font-semibold ${orderInfo?.orderId ? '' : 'opacity-30'}`}>Issue</p>
         </div>
         <div className="flex items-center flex-col" onClick={() => {
             if (orderInfo?.orderId && cancellable(orderInfo?.orderState || '')) {
