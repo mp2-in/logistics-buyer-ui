4db2aa07919a48bd4d78fad099c43950a4545c89
Charan Raj <charanraj12@gmail.com>
2024-10-16 16:48:31 +0530

    Feature/wallet balance (#6)
    * Show wallet balance
    
    * Changed wallet balance text
    
    * Bug fix
    
    * Updated font size

--- src/components/AccountDetails.tsx
+++ src/components/AccountDetails.tsx
@@ -5,7 +5,6 @@ import Button from "@components/Button"
 import checkIcon from "@assets/check.png"
 import phoneIcon from "@assets/phone.png"
 import emailIcon from "@assets/email.png"
-// import Input from "@components/Input"
 
 
 export default ({ open, onClose, onLogout, selectedAccount, accountIds, phoneNumber, switchAccount, email }: {
@@ -18,8 +17,6 @@ export default ({ open, onClose, onLogout, selectedAccount, accountIds, phoneNum
     email?: string
     switchAccount: (accountId: string) => void
 }) => {
-
-    // const [rechargeAmount, setRechargeAmount] = useState('')
 
     return <Modal open={open} onClose={onClose}>
         <div className={'bg-white rounded flex flex-col items-center p-5 w-[350px] relative md:w-[400px]'} onMouseDown={e => e.stopPropagation()}>
@@ -52,34 +49,6 @@ export default ({ open, onClose, onLogout, selectedAccount, accountIds, phoneNum
                         </div>
                     })}
                 </div>
-                {/* <div className="flex flex-col items-center *:mb-5">
-                    <Input label='Recharge Amount' type={'number'} size="small" value={rechargeAmount} onChange={val => /^[0-9]*$/.test(val) && setRechargeAmount(val)}/>
-                    <Button title="Recharge" variant="primary" onClick={() => {
-                        var options = {
-                            "key": "YOUR_KEY_ID", // Enter the Key ID generated from the Dashboard
-                            "amount": rechargeAmount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-                            "currency": "INR",
-                            "name": "MP2 Innovations", //your business name
-                            "description": "Wallet Recharge",
-                            "image": "https://example.com/your_logo",
-                            "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-                            "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-                            "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
-                                "name": "Gaurav Kumar", //your customer's name
-                                "email": "gaurav.kumar@example.com",
-                                "contact": "9000090000" //Provide the customer's phone number for better conversion rates 
-                            },
-                            "notes": {
-                                "address": "Razorpay Corporate Office"
-                            },
-                            "theme": {
-                                "color": "#3399cc"
-                            }
-                        };
-                        var rzp1 = (window as any).Razorpay(options);
-                        rzp1.open();
-                    }} disabled={!rechargeAmount}/>
-                </div> */}
                 <div className="absolute bottom-1 right-1">
                     <Button title="Logout" onClick={onLogout} variant="secondary" />
                 </div>
