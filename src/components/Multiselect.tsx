615f70bc58a8f300303e51267cc6be5f9080616a
Charan <charanraj12@gmail.com>
2024-10-17 15:59:53 +0530

    Reduced search box font

--- src/components/Multiselect.tsx
+++ src/components/Multiselect.tsx
@@ -58,7 +58,7 @@ export default <T extends unknown>({ label, value, onChange, size, options, requ
         <div ref={selectContainerRef} className={`absolute top-[33px] z-50 left-2 right-2 bg-white shadow-3xl rounded max-h-[250px] flex flex-col`}>
           {options.length >= 6 && !hideSearch ?
             <div className={'flex flex-row items-center mx-2'}>
-              <input placeholder='Search' value={searchFilter} onChange={e => setSearchFilter(e.target.value)} className="outline-none border bg-white rounded my-2 mx-3 px-2 font-sans h-8" />
+              <input placeholder='Search' value={searchFilter} onChange={e => setSearchFilter(e.target.value)} className="outline-none border bg-white rounded my-2 mx-3 px-2 font-sans h-8 text-sm" />
               <img src={close} onClick={() => setSearchFilter('')} className="w-6 cursor-pointer" />
             </div> : null}
           <div className="overflow-auto">
